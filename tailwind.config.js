fb3aa5e7013cfac4f63604aff8f28f2a7d87fde0
pankajmokashi <pankajmokashi12@gmail.com>
2024-11-17 00:42:00 +0530

    Revert "chore: install prop-types"
    This reverts commit 32c17c5725727e84c89cbee214b1d3a36753748b.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    screens: {
-      xm: "450px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
   },
   plugins: [],
 };
