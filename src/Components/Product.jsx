08421eb044320408d84a12844c19857bbd156565
pankajmokashi <pankajmokashi12@gmail.com>
2024-11-18 23:57:13 +0530

    feact(Product): add addToCart functionality to button

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,29 +1,38 @@
 import PropTypes from "prop-types";
+import { AppContext } from "../context/ProductsContext";
+import { useContext } from "react";
 
 function Product({ product }) {
+  const { addToCart } = useContext(AppContext);
+
   return (
-    <div className="mx-auto w-full xm:max-w-md md:max-w-full p-4 sm:p-8 lg:px-16 flex flex-col md:flex-row gap-8">
-      <div className="h-full">
-        <img
-          alt={product.name}
-          src={product.image}
-          className="w-full md:max-w-sm h-full rounded-md mx-auto shadow-lg"
-        />
-      </div>
-      <div className="md:pt-12 md:pl-8 pb-8">
-        <h2 className="text-xl xm:text-2xl sm:text-3xl font-bold mb-2 sm:mb-4">
-          {product.name}
-        </h2>
-        <p className="text-xs xm:text-sm sm:text-base mb-2 sm:mb-4 max-w-md">
-          {product.description}
-        </p>
-        <div className="text-base sm:text-lg xm:text-xl font-bold mb-4">
-          ${product.price}
+    <div className="w-full flex justify-center">
+      <div className="mx-auto w-full xm:max-w-md md:max-w-full p-4 sm:p-8 lg:px-16 flex flex-col justify-center md:flex-row gap-8">
+        <div className="h-full">
+          <img
+            alt={product.name}
+            src={product.image}
+            className="w-full md:max-w-sm h-full rounded-md mx-auto shadow-lg hover:scale-105"
+          />
         </div>
-        <div className="flex justify-center">
-          <button className="w-full max-w-md py-2 bg-black text-white font-medium rounded-md hover:opacity-80">
-            Add to cart
-          </button>
+        <div className="md:pt-12 md:pl-8 pb-8">
+          <h2 className="text-xl xm:text-2xl sm:text-3xl font-bold mb-2 sm:mb-4">
+            {product.name}
+          </h2>
+          <p className="text-xs xm:text-sm sm:text-base mb-2 sm:mb-4 max-w-md">
+            {product.description}
+          </p>
+          <div className="text-base sm:text-lg xm:text-xl font-bold mb-4">
+            ${product.price}
+          </div>
+          <div className="flex justify-center">
+            <button
+              onClick={() => addToCart(product)}
+              className="w-full max-w-md py-2 bg-black text-white font-medium rounded-md hover:opacity-80"
+            >
+              Add to cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
