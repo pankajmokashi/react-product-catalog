0d3d201013d20cea94f9ea98cb707fd77f9e97e5
pankajmokashi <pankajmokashi12@gmail.com>
2024-11-19 14:56:53 +0530

    feat(Header): add home link to CATALOG heading

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,9 +13,9 @@ function Header() {
       <header className="mx-auto max-w-screen-2xl px-4 xm:px-6 lg:px-8">
         <div className="flex items-center justify-between py-3 sm:py-4">
           <div>
-            <div className="text-2xl sm:text-4xl font-bold cursor-default">
-              CATALOG
-            </div>
+            <a href="/" className="text-2xl sm:text-4xl font-bold">
+              <h1>CATALOG</h1>
+            </a>
           </div>
 
           <div>
@@ -32,7 +32,7 @@ function Header() {
               >
                 <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
               </svg>
-              <span className="absolute text-xs top-[11px] right-[18px] sm:top-[10px] sm:right-[20px] sm:text-sm text-black font-semibold">
+              <span className="absolute text-[11px] top-[11px] right-[18px] sm:top-[12px] sm:right-[20px] sm:text-[12px] text-black font-bold">
                 {totalProducts}
               </span>
             </button>
