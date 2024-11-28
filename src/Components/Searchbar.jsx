7e6700d75606374c431359fb81544eb57873b65d
pankajmokashi <pankajmokashi12@gmail.com>
2024-11-28 23:24:38 +0530

    feat(Searchbar): add search button and update search functionality

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,19 +1,39 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../context/ProductsContext";
 
 function Searchbar() {
-  const { searchValue, setSearchValue } = useContext(AppContext);
+  const [value, setValue] = useState("");
+  const { setSearchValue } = useContext(AppContext);
 
   return (
     <>
       <input
         type="text"
         name="search-product"
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         placeholder="Search..."
-        className="px-3 py-2 flex-grow rounded-md bg-black text-white outline-none"
+        className="px-3 py-2 flex-grow rounded-md bg-white border border-black outline-none"
       />
+      <button
+        onClick={() => setSearchValue(value)}
+        className="px-3 py-2 rounded-md border bg-black text-white"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
+      </button>
     </>
   );
 }
