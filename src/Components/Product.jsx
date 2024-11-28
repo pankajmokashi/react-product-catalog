14e5a62cb8abdda101a48891f3f3ea64554e311e
pankajmokashi <pankajmokashi12@gmail.com>
2024-11-28 21:32:40 +0530

    feat(Product): Update product component to display details in modal

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -6,33 +6,41 @@ function Product({ product }) {
   const { addToCart } = useContext(AppContext);
 
   return (
-    <div className="w-full flex justify-center">
-      <div className="mx-auto w-full xm:max-w-md md:max-w-full p-4 sm:p-8 lg:px-16 flex flex-col justify-center md:flex-row gap-8">
-        <div className="h-full">
-          <img
-            loading="lazy"
-            alt={product.name}
-            src={product.image}
-            className="w-full md:max-w-sm h-full rounded-md mx-auto shadow-lg hover:scale-105"
-          />
-        </div>
-        <div className="md:pt-12 md:pl-8 pb-8">
-          <h2 className="text-xl xm:text-2xl sm:text-3xl font-bold mb-2 sm:mb-4">
+    <div className="flex flex-col items-center mx-auto">
+      <div>
+        <div className="mb-6">
+          <h2 className="text-xl xm:text-2xl sm:text-3xl font-bold mb-2">
             {product.name}
           </h2>
-          <p className="text-xs xm:text-sm sm:text-base mb-2 sm:mb-4 max-w-md">
-            {product.description}
-          </p>
-          <div className="text-base sm:text-lg xm:text-xl font-bold mb-4">
-            ${product.price}
+          <span className="px-6 pt-1 pb-2 bg-gray-200 rounded-lg text-sm font-medium text-slate-700">
+            {product.category}
+          </span>
+        </div>
+        <div className="w-full xm:max-w-md md:max-w-full p-4 flex flex-col justify-center md:flex-row gap-8">
+          <div className="mx-auto max-w-[300px] h-full overflow-hidden rounded-md">
+            <img
+              loading="lazy"
+              alt={product.name}
+              src={product.image}
+              className="w-full h-full shadow-lg hover:scale-110 transition-all"
+            />
           </div>
-          <div className="flex justify-center">
-            <button
-              onClick={() => addToCart(product)}
-              className="w-full max-w-md py-2 bg-black text-white font-medium rounded-md hover:opacity-80"
-            >
-              Add to cart
-            </button>
+          <div className="max-w-sm md:pl-8 pb-8">
+            <div className="text-base sm:text-lg xm:text-xl font-bold mb-2">
+              ${product.price}
+            </div>
+            <p className="text-xs xm:text-sm sm:text-base mb-2 sm:mb-4 text-gray-600">
+              {product.description}
+            </p>
+
+            <div className="flex justify-center">
+              <button
+                onClick={() => addToCart(product)}
+                className="w-full py-2 text-xs sm:text-sm font-medium rounded-md border border-black hover:bg-black hover:text-white"
+              >
+                Add to cart
+              </button>
+            </div>
           </div>
         </div>
       </div>
