196c66355e88a64f270dd07ce64e590dabb39bfa
pankajmokashi <pankajmokashi12@gmail.com>
2024-11-28 20:05:12 +0530

    feat(ProductList): Add add-to-cart button

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../context/ProductsContext";
 
 function ProductList({ products }) {
+  const { addToCart } = useContext(AppContext);
 
   return (
     <div className="mx-auto px-4 sm:px-6 lg:px-8">
       <div className="mt-2 sm:mt-6 grid grid-cols-1 gap-x-6 gap-y-12 xm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 xl:gap-x-8">
         {products.map((product) => (
-          <Link to={`/product/${product.id}`} key={product.id}>
-            <div className="group relative">
+            <div key={product.id} className="group relative">
               <img
                 loading="lazy"
                 alt={product.name}
@@ -23,9 +24,15 @@ function ProductList({ products }) {
                 <button className="w-full py-2 text-sm bg-black text-white rounded-lg group-hover:opacity-75">
                   View Details
                 </button>
+                <button
+                onClick={() => addToCart(product)}
+                className="w-full py-2 text-sm rounded-lg border border-black hover:bg-black hover:text-white"
+              >
+                Add to Cart
+              </button>
+
               </div>
             </div>
-          </Link>
         ))}
       </div>
     </div>
